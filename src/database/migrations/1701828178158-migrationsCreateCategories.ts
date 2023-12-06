ec8d1d08c1998ddb3f7072640ba85dba86682034
maysilsantos <maysilsantos@yahoo.com.br>
2023-12-06 00:05:14 -0300

    ERR: PROBLEMAS PARA CONFIGURAR TYPEORM

---
+++ src/database/migrations/1701828178158-migrationsCreateCategories.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class MigrationsCreateCategories1701828178158
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "categories",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "id",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("categories")
+  }
+}
