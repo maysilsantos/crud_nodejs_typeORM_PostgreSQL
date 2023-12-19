3359d9ee73130c26dc3c82b4fc1e2cad8cc35ccd
maysilsantos <maysilsantos@yahoo.com.br>
2023-12-18 23:22:09 -0300

    definicao de migrations

---
+++ src/entities/Room.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Video } from "./Video";
+
+@Entity(`rooms`)
+export class Room {
+   @PrimaryGeneratedColumn()
+   id: number
+   
+   @Column({
+      type: "text"
+   })
+   name: string
+
+   @OneToMany(() => Video, (video) => video.room )
+   videos: Video[]
+
+}
